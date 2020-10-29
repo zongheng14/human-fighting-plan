a1dc2d8f334d9e4c63bf19300928569e969bcaa2
ZX-1401-0093 <shaonan.hu@baodanyun-inc.com>
2020-10-29 15:50:36 +0800

    # 插入排序

--- human-algorithmic-and-datastructure/src/main/java/com/human/algorithmic/八大排序算法/C_插入排序/InsertSort.java
+++ human-algorithmic-and-datastructure/src/main/java/com/human/algorithmic/八大排序算法/C_插入排序/InsertSort.java
@@ -6,4 +6,11 @@ package com.human.algorithmic.八大排序算法.C_插入排序;
  * @Time 2020/10/29
  */
 public class InsertSort {
+
+    /**
+     * 通过交换进行插入排序，借鉴冒泡排序
+     */
+    public static void sort(int[] arr) {
+
+    }
 }
